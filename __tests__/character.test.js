6a8029c9f90d4047e81f15a7d443d14e21919bfc
Marcus <marcusferreira23@gmail.com>
2022-03-06 12:40:54 -0800

    pass fifth test

--- __tests__/character.test.js
+++ __tests__/character.test.js
@@ -19,10 +19,16 @@ describe('Character', () => {
     const character = new Character('Spike', 50, 70);
     expect(character.strength).toEqual(70);
   });
-  
+
   test('should correctly return the character object with attack', () =>
   {
     const character = new Character('Spike', 50, 70, 'Chain');
     expect(character.attack).toEqual('Chain');
+  });
+
+  test('should correctly return the character object with special ability', () =>
+  {
+    const character = new Character('Spike', 50, 70, 'Chain', 'Sunlight');
+    expect(character.specialAbility).toEqual('Sunlight');
   });
 });
