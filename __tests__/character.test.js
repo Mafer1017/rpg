80c9c2db54b13f4e632d524791d0b841b8aed5bc
Marcus <marcusferreira23@gmail.com>
2022-03-06 16:07:13 -0800

    dayWalker passes first and second test

--- __tests__/character.test.js
+++ __tests__/character.test.js
@@ -2,22 +2,6 @@ import Character from './../src/js/character.js'
 
 describe('Character', () => {
 
-  // test('should correctly create a character object with a name', () => {
-  //   const character = new Character('Spike');
-  // });
-  
-  // test('should correctly return the character object with health', () => {
-  //   const character = new Character('Spike', 50);
-  // });
-  
-  // test('should correctly return the character object with strength', () => {
-  //   const character = new Character('Spike', 50, 70);
-  // });
-  
-  // test('should correctly return the character object with attack', () => {
-  //   const character = new Character('Spike', 50, 70, 'Chain');
-  // });
-  
   test('should correctly return the character object with special ability', () => {
     const character = new Character('Spike', 50, 70, 'Chain', 'Sunlight');
     expect(character.name).toEqual('Spike');
@@ -31,10 +15,11 @@ describe('Character', () => {
 
 describe('dayWalker', () => {
 
-  test('should correctly create a dayWalker object with a familiar', () => {
+  test('should correctly create a dayWalker object with a familiar, and blood', () => {
     const character = new Character('Spike', 50, 70, 'Chain', 'Sunlight');
     character.dayWalker();
     expect(character.familiar).toEqual(0);
+    expect(character.blood).toEqual(" ");
   });
 
 });
